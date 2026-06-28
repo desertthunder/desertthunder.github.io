3fac5b33d2e948dfc2218b17bfe7004a75e1cbba
Owais Jamil <desertthunder.dev@gmail.com>
2026-06-28 00:01:09 -0500

    chore: remove editor conf
    * update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 import { getIconCollections, iconsPlugin } from "@egoist/tailwindcss-icons";
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: "class",
-	content: ["./lib/**/*.{html,js}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		iconsPlugin({
-			collections: getIconCollections([
-				"ri",
-				"devicon",
-				"simple-icons",
-				"file-icons",
-				"mdi",
-			]),
-		}),
-	],
+    darkMode: "class",
+    content: ["./lib/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        iconsPlugin({
+            collections: getIconCollections([
+                "ri",
+                "devicon",
+                "simple-icons",
+                "file-icons",
+                "mdi",
+            ]),
+        }),
+    ],
 };
